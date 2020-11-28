0d8a6edec2f6fdadb6771938d5a8bc9a89233fac
Ahm A <asad.gooogle@gmail.com>
2020-11-28 00:53:57 -0500

    Update credentials.h

--- Sniffer/credentials.h
+++ Sniffer/credentials.h
@@ -1,3 +1,8 @@
 
 const char* ssid = "";
 const char* password = "";
+const char* mqttUser = "";
+const char* mqttPassword = "";
+
+const char* routerMAC = "1c:1d";
+const char *targetDevices[] = {""ab:cd","ef:gh"};
